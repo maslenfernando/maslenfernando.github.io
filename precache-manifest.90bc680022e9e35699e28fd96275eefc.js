83e886cdd07caa881ad3b40ed2fbefe70eddce97
maslen fernando <maslaenfernandohutabarat@gmail.com>
2021-06-15 14:43:42 +0700

    Menyalin isi folder build

---
+++ precache-manifest.90bc680022e9e35699e28fd96275eefc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3094cac78e2dafaaacb",
+    "url": "/static/css/main.00f6269e.chunk.css"
+  },
+  {
+    "revision": "b3094cac78e2dafaaacb",
+    "url": "/static/js/main.b3094cac.chunk.js"
+  },
+  {
+    "revision": "6c3a4e84c9da792bfb03",
+    "url": "/static/js/1.6c3a4e84.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b5d58b3c100ff8d4698ff93034e6d278",
+    "url": "/index.html"
+  }
+];
